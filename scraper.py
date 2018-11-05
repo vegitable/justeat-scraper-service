64570094ddc3654302e38f2c998999b19795fae1
Muzzi Aldean <muzzialdean@APPL-C02RD2EXFVH6.oslcorp.overseasstudentliving.com>
2018-11-05 17:58:58 +0000

    update scraper

--- scraper.py
+++ scraper.py
@@ -1,28 +1,40 @@
-from urllib.request import urlopen
+import urllib.request
 from bs4 import BeautifulSoup
+import certifi
+import csv
 
 class JustEatScraper:
 
-url = "https://www.just-eat.co.uk/area/"
+    header_data = ['User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36']
+    restaurants = []
+    menu_slugs = []
 
-page = urlopen(url)
+    def pull_menu_slugs_from_csv(self):
+        with open('je_menu_urls.csv', 'r') as csvfile:
+            slug_reader = csv.reader(csvfile)
+            for row in slug_reader:
+                self.menu_slugs.append(row[0])
 
-soup = BeautifulSoup(page, 'html.parser')
+    def scrape_restaurant(self, url):
+        req = urllib.request.Request(url)
+        req.add_header(self.header_data[0], self.header_data[1])
+        page = urllib.request.urlopen(req)
+        soup = BeautifulSoup(page, 'html.parser')
+        name_box = soup.find('h4', attrs={'class': 'name'})
+        name = name_box.text.strip()
+        description_box = soup.find('p', attrs={'class':'description'})
+        description = description_box.text
+        price_box = soup.find('p', attrs={'class':'price'})
+        price = price_box.text
+        restaurant = {
+            'name': name,
+            'description': description,
+            'price': price,
+        }
+        self.restaurants.append(restaurant)
 
-name_box = soup.find('h4', attrs={'class': 'name'})
-
-name = name_box.text.strip()
-
-print(name) 
-
-description_box = soup.find('p', attrs={'class':'description'})
-
-description = description_box.text
-
-print(description)
-
-price_box = soup.find('p', attrs={'class':'price'})
-
-price = price_box.text
-
-print(price)
+scraper = JustEatScraper()
+scraper.pull_menu_slugs_from_csv()
+url = "https://www.just-eat.co.uk/restaurants-Mario-Pizza-E14/menu"
+scraper.scrape_restaurant(url)
+print(scraper.restaurants)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='myproject',
+      version='1.0',
+      packages=find_packages(),
+      install_requires=[
+        'beautifulsoup4==4.6.3',
+        'selenium==3.14.1',
+        'certifi==2018.10.15',
+    ],
+)
